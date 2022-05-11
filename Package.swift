c106a3662adf11a31dbdc8f6c64e080e0e24cc4e
f0enix <72214428+f0enix@users.noreply.github.com>
2022-05-11 12:47:55 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "AlertKit",
     platforms: [
         .iOS(.v14),
-        .watchOS(.v7)
+        .watchOS(.v7),
+        .macOS(.v10_15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
